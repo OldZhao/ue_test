b9106148f0d2c80eb49c64a8a93f9c1b0fdb35ea
OldZhao <frankpu.yf@gmail.com>
2022-03-12 22:53:43 +0800

    add code to save input

--- save_color.py
+++ save_color.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import os
 
 import OpenEXR
 import Imath
@@ -15,28 +16,28 @@ def binaryToEXR(file_name, exr_name, rows, cols, layer):
     else:
         print("error layer:{}".format(layer))
     
-    color = np.float32(color)
+    color = np.float16(color)
     color = color.reshape((rows, cols, -1))
     print("layer:{}, color.shape:{}, color.dtype:{}".format(layer, color.shape, color.dtype))
     
     if layer == 'depth':
         color = np.squeeze(color)
-        type_chan = Imath.Channel(Imath.PixelType(Imath.PixelType.FLOAT))
+        type_chan = Imath.Channel(Imath.PixelType(Imath.PixelType.HALF))
         HEADER = OpenEXR.Header(color.shape[1], color.shape[0])
         HEADER['channels'] = dict([(c, type_chan) for c in "R"])
         exr = OpenEXR.OutputFile(exr_name, HEADER)
-        color = color / np.max(color) * 255
+        # color = color / np.max(color) * 255
         exr.writePixels({"R": np.squeeze(color)})
         exr.close()
     elif layer == "velocity":
-        type_chan = Imath.Channel(Imath.PixelType(Imath.PixelType.FLOAT))
+        type_chan = Imath.Channel(Imath.PixelType(Imath.PixelType.HALF))
         HEADER = OpenEXR.Header(color.shape[1], color.shape[0])
         HEADER['channels'] = dict([(c, type_chan) for c in "GR"])
         exr = OpenEXR.OutputFile(exr_name, HEADER)
         exr.writePixels({"G": np.squeeze(color[:,:,0]/1), "R":np.squeeze(color[:,:,1]/1)})
         exr.close()
     elif layer in ["input", "output"]:
-        type_chan = Imath.Channel(Imath.PixelType(Imath.PixelType.FLOAT))
+        type_chan = Imath.Channel(Imath.PixelType(Imath.PixelType.HALF))
         HEADER = OpenEXR.Header(color.shape[1], color.shape[0])
         HEADER['channels'] = dict([(c, type_chan) for c in "RGB"])
         exr = OpenEXR.OutputFile(exr_name, HEADER)
@@ -46,21 +47,35 @@ def binaryToEXR(file_name, exr_name, rows, cols, layer):
 
 if __name__ == "__main__":
 
-    start = 20
-    end = start + 10
-    for count in range(start, end):
-        layers = ['output', 'input']
-        for layer in layers:
+    start = 1
+    end = start + 176
+    rows1 = 720
+    cols1 = 1280
+    rows2 = 720
+    cols2 = 1280
+
+
+    root_path = "e:/DLSS/data/TAA"
+    folder_path = "03_12_22_32"
+    input_root = "{}/raw/{}".format(root_path, folder_path)
+    output_root = "{}/exr/{}".format(root_path, folder_path)
+    os.makedirs(output_root, exist_ok=True)
+
+
+    # layers = ['output', 'input']
+    layers = ['input']
+    for layer in layers:
+        for count in range(start, end):
             print(layer)
             if layer == "velocity":
-                rows = 702
-                cols = 1171
+                rows = rows2
+                cols = cols2
             elif layer == "output":
-                rows = 564
-                cols = 1072
+                rows = rows2
+                cols = cols2
             elif layer == "input":
-                rows = 282
-                cols = 536
-            file_name = "d:/pc_code/data/map_DLSS_{}_{}_{}_{}.txt".format(count, cols, rows, layer)
-            exr_name = file_name.replace(".txt", ".exr")
+                rows = rows1
+                cols = cols1
+            file_name = "{}/{}_{}_{}_{}.txt".format(input_root, count, cols, rows, layer)
+            exr_name = "{}/{}_{}_{}_{}.exr".format(output_root, count, cols, rows, layer)
             binaryToEXR(file_name, exr_name, rows, cols, layer)
